9eca98bdc03d885ca8482ef591dda433b644be61
wanxk <wanxk@asiainfo.com>
2020-09-05 21:15:29 +0800

    初始化项目

---
+++ src/main/java/com/ecit/st/core/StCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ecit.st.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecit/st/core/StCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecit.st.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
